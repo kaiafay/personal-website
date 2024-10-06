06a2cd95ed72bd79d279f700577cf42a8a917e45
Kaia Scheirman <kfscheirman18@gmail.com>
2024-10-06 16:15:25 -0700

    style buttons

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,23 @@ import Contact from "./pages/Contact";
 import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 
 export default function App() {
+  const buttonStyle = {
+    transition: "transform 0.2s",
+    backgroundColor: "transparent",
+    "&:hover": {
+      transform: "scale(1.1)",
+      backgroundColor: "transparent",
+    },
+    "&:active": {
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    },
+    "&:focus": {
+      outline: "none",
+      boxShadow: "none",
+    },
+  };
+
   return (
     <div>
       <AppBar
@@ -16,25 +33,55 @@ export default function App() {
         <Toolbar sx={{ minHeight: 80, height: "100%", alignItems: "center" }}>
           <Typography
             variant="button"
+            component={Link}
+            to="/"
             sx={{
               flexGrow: 1,
               color: "white",
               fontSize: "1.8rem",
               textTransform: "uppercase",
+              textDecoration: "none",
+              "&:hover": {
+                cursor: "pointer",
+              },
             }}
           >
             Kaia Fay
           </Typography>
-          <Button color="inherit" component={Link} to="/">
+          <Button
+            color="inherit"
+            component={Link}
+            to="/"
+            sx={buttonStyle}
+            disableRipple
+          >
             Home
           </Button>
-          <Button color="inherit" component={Link} to="/resume">
+          <Button
+            color="inherit"
+            component={Link}
+            to="/resume"
+            sx={buttonStyle}
+            disableRipple
+          >
             Resume
           </Button>
-          <Button color="inherit" component={Link} to="/about">
+          <Button
+            color="inherit"
+            component={Link}
+            to="/about"
+            sx={buttonStyle}
+            disableRipple
+          >
             About Me
           </Button>
-          <Button color="inherit" component={Link} to="/contact">
+          <Button
+            color="inherit"
+            component={Link}
+            to="/contact"
+            sx={buttonStyle}
+            disableRipple
+          >
             Contact
           </Button>
         </Toolbar>
