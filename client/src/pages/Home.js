b1080a00d08914936e89e51be2e70e78fc3793a2
Kaia Scheirman <kfscheirman18@gmail.com>
2024-10-08 16:34:11 -0700

    stacked card effect on profile photo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography, Box } from "@mui/material";
 import myPhoto from "../assets/avatar.jpg";
 import { Typewriter } from "react-simple-typewriter";
+
 export default function Home() {
   return (
     <Box
@@ -12,7 +13,20 @@ export default function Home() {
         gap: 4,
       }}
     >
-      <Box sx={{ flexShrink: 0 }}>
+      <Box sx={{ position: "relative", flexShrink: 0 }}>
+        <Box
+          sx={{
+            width: 200,
+            height: 250,
+            backgroundColor: "black",
+            borderRadius: 2,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            transform: "rotate(-6deg)",
+            zIndex: 1,
+          }}
+        />
         <Box
           component="img"
           src={myPhoto}
@@ -22,6 +36,8 @@ export default function Home() {
             height: 250,
             objectFit: "cover",
             borderRadius: 2,
+            position: "relative",
+            zIndex: 2,
           }}
         />
       </Box>
